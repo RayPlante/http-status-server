7581e84444d0187bbfaf40847363c20ef86d379f
Ray Plante <raymond.plante@nist.gov>
2025-11-29 03:11:12 -0500

    make tests runnable from setup.py, testall.sh

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
+from unittest import TestLoader
 
 pkgname = 'http_status_server'
+
+def discover_tests():
+    return TestLoader().discover('tests', pattern='test_*.py')
 
 setup(
     packages=find_packages(
         include=[pkgname]
     ),
+    test_suite='setup.discover_tests'
 )
