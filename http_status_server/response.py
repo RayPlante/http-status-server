a35e6b765dec579c4fb7e01223c1990a5898377e
Ray Plante <raymond.plante@nist.gov>
2025-11-29 02:14:35 -0500

    introduce response module, Resource class

---
+++ http_status_server/response.py
@@ -0,0 +1,212 @@
+"""
+module defining main interfaces and implementations for managing responses
+"""
+import json, io
+from abc import ABC, abstractmethod, abstractproperty
+from typing import Mapping, List
+from collections import OrderedDict
+from datetime import datetime
+from copy import deepcopy
+
+from http_status import Status
+from urllib3 import HTTPResponse
+import requests
+from requests.utils import get_encoding_from_headers
+
+GET    = "GET"
+POST   = "POST"
+PUT    = "PUT"
+DELETE = "DELETE"
+OPTIONS= "OPTIONS"
+HEAD   = "HEAD"
+MKCOL  = "MKCOL"
+FIND   = "FIND"
+
+
+class Resource(ABC):
+    """
+    an abstract base class that generates different responses for a particular 
+    resource path.
+    """
+
+    def __init__(self, version: int = 1, version_label: str = "HTTP/1.1"):
+        self._ver = version
+        self._verstr = version_label
+
+    @property
+    def version(self):
+        """The HTTP version as understood by the ``urlllib3`` module (defaults to 1)"""
+        return self._ver
+
+    @property
+    def version_string(self):
+        """The HTTP version label to appear in a response's status line (e.g. "HTTP/1.1")"""
+        return self._verstr
+
+    @abstractmethod
+    def get_response_to(self, method: str, status: int = 200) -> HTTPResponse:
+        """
+        return an HTTP-compliant response, including the header.
+        :param str method:  the desired HTTP method (e.g. "GET")
+        :param int status:  the desired HTTP status code (default: 200)
+        """
+        raise NotImplementedError()
+
+    def respond(self, method: str = "GET", status: int = 200) -> str:
+        """
+        return a formatted (newline-delimited) HTTP response
+        """
+        pass
+
+class InMemoryResource(Resource):
+    """
+    A resource where the response is configured internally
+    """
+
+    def __init__(self, config: Mapping = None):
+        super(InMemoryResource, self).__init__()
+        if config is None:
+            config = {}
+        self._data = config
+        self._mkbody = {
+            'bytes': self._make_bytes_body,
+            'text':  self._make_text_body,
+            'json':  self._make_json_body
+        }
+
+    def get_response_to(self, method: str, status: int = 200, datestr = None) -> HTTPResponse:
+        if not datestr:
+            datestr = datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S %Z")
+        hdrs = OrderedDict([('Date', datestr)])
+        
+        hdrs.update(self._data.get('headers', OrderedDict()))
+
+        bymeth = self._data.get(method)
+        if not bymeth:
+            bymeth = self._data.get("def", {})
+
+        hdrs.update(bymeth.get('headers', {}))
+
+        bystat = bymeth.get(status)
+        if not bystat:
+            bystat = bymeth.get('def', {})
+        hdrs.update(bystat.get('headers', {}))
+
+        return self._make_resp(method, status, hdrs, bystat)
+
+    def get_requests_response_to(self, method: str, status: int = 200,
+                                 datestr = None) -> requests.Response:
+        return to_requests_response(self.get_response_to(method, status, datestr))
+
+    def _make_resp(self, method: str, status: int, headers: Mapping, respcfg: Mapping):
+        stat = Status(status)
+        ct = headers.get("Content-Type")
+        return HTTPResponse(self._make_body(method, status, respcfg, ct), 
+                            headers, status, self.version, self.version_string, stat.name)
+
+    def _make_body(self, method, status, respdata, contenttype: str=None):
+        if method == "HEAD":
+            return self._make_empty_body(method, status)
+
+        if not contenttype:
+            contenttype = "def"
+        bdata = respdata.get('body', {})
+        if bdata:
+            if bdata.get(contenttype) is None and contenttype != "def":
+                bdata = bdata.get("def")
+            else:
+                bdata = bdata.get(contenttype)
+        if not bdata:
+            return self._make_empty_body(method, status)
+
+        btype = bdata.get('type', 'text')
+        return self._mkbody.get(btype, self._make_bytes_body)(method, status, bdata)
+
+    def _make_empty_body(self, method, status, *args):
+        return io.BytesIO(b'')
+
+    def _make_text_body(self, method, status, bodydata):
+        body = bodydata.get('content', '')
+        enc = bodydata.get('encoding', 'utf-8')
+        if isinstance(body, str):
+            try:
+                body = body.encode(enc)
+            except Exception as ex:
+                raise ConfigError(f"Bad body encoding string for {method}/{status}")
+        return io.BytesIO(body)
+
+    def _make_bytes_body(self, method, status, bodydata):
+        body = bodydata.get('content', '')
+        if isinstance(body, str):
+            enc = bodydata.get('encoding', 'utf-8')
+            try:
+                body = body.encode(enc)
+            except Exception as ex:
+                raise ConfigError(f"Bad body encoding string for {method}/{status}")
+        return io.BytesIO(body)
+
+    def _make_json_body(self, method, status, bodydata):
+        return io.BytesIO(json.dumps(bodydata.get('content')).encode('utf-8'))
+
+_simple_config = {
+    "headers": OrderedDict([
+        ("Server", "http-status-server"),
+        ("Content-Type", "text/plain"),
+    ])
+}
+
+def _merge_config(updates: Mapping, defconf: Mapping) -> Mapping:
+    # knicked from oar-metadata (https://github.com/usnistgov/oar-metadata),
+    # nistoar.base.config.merge_config()
+    out = deepcopy(defconf)
+    for key in updates:
+        if isinstance(updates[key], Mapping) and \
+           isinstance(out.get(key), Mapping):
+            out[key] = _merge_config(updates[key], out[key])
+        else:
+            out[key] = updates[key]
+    return out
+
+def to_requests_response(response: HTTPResponse, url: str = '/') -> requests.Response:
+    """
+    convert requests.Response object.
+    """
+    # adapted from requests.adapters.HTTPAdapter.build_response()
+    out = requests.Response()
+    out.status_code = response.status
+    out.encoding = get_encoding_from_headers(response.headers) or "utf-8"
+
+    if isinstance(response._body, str):
+        out.raw = io.BytesIO(response._body.encode(out.encoding))
+    elif isinstance(response._body, bytes):
+        out.raw = io.BytesIO(response._body)
+    elif isinstance(response._fp, io.BytesIO):
+        out.raw = io.BytesIO(response._fp.getvalue())
+    elif response._fp:
+        out.raw = response._fp
+    else:
+        out.raw = None
+            
+    out.reason = response.reason
+    out.url = url
+
+    return out
+    
+
+class SimpleResource(InMemoryResource):
+    """
+    A simple implementation of a resource that focuses primarily on returning a 
+    response with the requested status.  
+
+    It is possible to configure this class for specific headers and bodies (as with
+    :py:class:`InMemoryResource`, but it is not required.  
+    """
+
+    def __init__(self, config: Mapping = None):
+        if config is None:
+            config = _simple_config
+        else:
+            config = _merge_config(config, _simple_config)
+
+        super(SimpleResource, self).__init__(config)
+
---
+++ tests/http_status_server/test_response.py
@@ -0,0 +1,202 @@
+import unittest, json, io
+
+import urllib3, requests
+
+from http_status_server.response import SimpleResource
+
+bodycfg = {
+    "def": {
+        "type": "text",
+        "content": b"it's default"
+    },
+    "text/json": {
+        "type": "json",
+        "content": { "a": 1, "b": 2 }
+    },
+    "application/octet-stream": {
+        "type": "bytes",
+        "content": b"it's bytes"
+    },
+    "text/plain": {
+        "type": "text",
+        "content": "it's text"
+    }
+}
+
+class TestSimpleResource(unittest.TestCase):
+
+    def setUp(self):
+        self.res = SimpleResource()
+
+    def test_ctor(self):
+        self.assertEqual(self.res.version, 1)
+        self.assertEqual(self.res.version_string, "HTTP/1.1")
+        self.assertTrue(self.res._data)
+        self.assertEqual(self.res._data.get("headers", {}).get("Server"),
+                         "http-status-server")
+        self.assertEqual(self.res._data.get("headers", {}).get("Content-Type"),
+                         "text/plain")
+
+    def test_make_empty_body(self):
+        self.assertTrue(isinstance(self.res._make_empty_body("GET", 200, {}), io.BytesIO))
+        self.assertEqual(self.res._make_empty_body("GET", 200).getvalue(), b'')
+        self.assertEqual(self.res._make_empty_body("PUT", 500).getvalue(), b'')
+        self.assertEqual(self.res._make_empty_body(None, 0).getvalue(), b'')
+
+    def test_make_text_body(self):
+        self.assertTrue(isinstance(self.res._make_text_body("GET", 200, {}), io.BytesIO))
+        self.assertEqual(self.res._make_text_body("GET", 200, {}).getvalue(), b'')
+        bcfg = { 'content': 'goob' }
+        self.assertEqual(self.res._make_text_body("GET", 200, bcfg).getvalue(), b'goob')
+        bcfg = { 'content': b'goob' } 
+        self.assertEqual(self.res._make_text_body("GET", 200, bcfg).getvalue(), b'goob')
+        bcfg = { 'content': 'goob'.encode('utf-16'), 'encoding': 'utf-16' } 
+        self.assertEqual(self.res._make_text_body("GET", 200, bcfg).getvalue(),
+                         'goob'.encode('utf-16'))
+
+    def test_make_bytes_body(self):
+        self.assertTrue(isinstance(self.res._make_bytes_body("GET", 200, {}), io.BytesIO))
+        self.assertEqual(self.res._make_bytes_body("GET", 200, {}).getvalue(), b'')
+        bcfg = { 'content': b'goob' }
+        self.assertEqual(self.res._make_bytes_body("PUT", 500, bcfg).getvalue(), b'goob')
+        bcfg = { 'content': 'goob' } 
+        self.assertEqual(self.res._make_bytes_body("GET", 200, bcfg).getvalue(), b'goob')
+        bcfg = { 'content': 'goob', 'encoding': 'utf-16' } 
+        self.assertEqual(self.res._make_bytes_body("GET", 200, bcfg).getvalue().
+                                                                     decode('utf-16'), 'goob')
+
+    def test_make_json_body(self):
+        self.assertTrue(isinstance(self.res._make_json_body("GET", 200, {}), io.BytesIO))
+        self.assertEqual(self.res._make_json_body("GET", 200, {}).getvalue(), b'null')
+        self.assertEqual(self.res._make_json_body("GET", 200, {"content": None}).getvalue(),
+                         b'null')
+        self.assertEqual(self.res._make_json_body("GET", 200, {"content": 3}).getvalue(), b'3')
+        self.assertEqual(self.res._make_json_body("GET", 200, {"content": "goob"}).getvalue(),
+                         b'"goob"')
+        self.assertEqual(self.res._make_json_body("GET", 200, {"content": [1, 3]}).getvalue(),
+                         b'[1, 3]')
+
+    def test_make_body(self):
+        cfg = { "body": bodycfg }
+        self.assertEqual(self.res._make_body("HEAD", 202, cfg).getvalue(), b'')
+        self.assertEqual(self.res._make_body("GET", 200, cfg).getvalue(), b"it's default")
+        self.assertEqual(self.res._make_body("PUT", 400, cfg, "text/plain").getvalue(),
+                         b"it's text")
+        self.assertEqual(self.res._make_body("GOOB", 405, cfg, "application/octet-stream").
+                                                                                    getvalue(),
+                         b"it's bytes")
+        self.assertEqual(self.res._make_body("POST", 500, cfg, "application/json").getvalue(),
+                         b"it's default")
+        self.assertEqual(self.res._make_body("PUT", 400, cfg, "text/json").getvalue(),
+                         b'{"a": 1, "b": 2}')
+        self.assertEqual(self.res._make_body("GET", 200, {"body": {}}).getvalue(), b'')
+        self.assertEqual(self.res._make_body("GET", 200, {"body": {"def":{}}}).getvalue(), b'')
+        
+    def test_get_response_to(self):
+        resp = self.res.get_response_to("GET", 200)
+        self.assertTrue(isinstance(resp, urllib3.HTTPResponse))
+        self.assertEqual(resp.status, 200)
+        self.assertEqual(resp.headers.get("Content-Type"), "text/plain")
+        self.assertEqual(resp.headers.get("Server"), "http-status-server")
+        self.assertEqual(resp.data, b'')
+
+        resp = self.res.get_response_to("HEAD", 404)
+        self.assertEqual(resp.status, 404)
+        self.assertEqual(resp.headers.get("Content-Type"), "text/plain")
+        self.assertEqual(resp.headers.get("Server"), "http-status-server")
+        self.assertEqual(resp.data, b'')
+
+    def test_get_response_to_with_body(self):
+        cfg = {
+            "def": {
+                "headers": {
+                    "X-HTTP-Status-Server": "true"
+                },
+                "def": {
+                    "body": bodycfg
+                }
+            },
+            "POST": {
+                "headers": {
+                    "X-HTTP-Status-Server": "false"
+                },
+                200: {
+                    "headers": {
+                        "Cache-Control": "max-age=1",
+                        "Content-Type": "text/json"
+                    },
+                    "body": {
+                        "text/json": { "type": "json", "content": { "id": 1 } }
+                    }
+                },
+                400: {
+                    "headers": {
+                        "Cache-Control": "max-age=10"
+                    },
+                    "body": {
+                        "def": { "type": "text", "content": "Ouch!" }
+                    }
+                }
+            },
+            "GOOB": {
+                "def": {
+                    "body": { "def": { "type": "text", "content": "Goob!" } }
+                }
+            }
+        }
+        self.res = SimpleResource(cfg)
+
+        resp = self.res.get_response_to("GET", 200)
+        self.assertEqual(resp.status, 200)
+        self.assertEqual(resp.headers.get("Server"), "http-status-server")
+        self.assertEqual(resp.headers.get("X-HTTP-Status-Server"), "true")
+        self.assertIn('Date', resp.headers)
+        self.assertNotIn('Cache-Control', resp.headers)
+        self.assertEqual(resp.headers.get("Content-Type"), "text/plain")
+        self.assertEqual(resp.data, b"it's text")
+
+        resp = self.res.get_response_to("POST", 400)
+        self.assertEqual(resp.status, 400)
+        self.assertEqual(resp.headers.get("Server"), "http-status-server")
+        self.assertEqual(resp.headers.get("X-HTTP-Status-Server"), "false")
+        self.assertEqual(resp.headers.get("Cache-Control"), "max-age=10")
+        self.assertIn('Date', resp.headers)
+        self.assertEqual(resp.headers.get("Content-Type"), "text/plain")
+        self.assertEqual(resp.data, b"Ouch!")
+
+        resp = self.res.get_response_to("POST", 200)
+        self.assertEqual(resp.status, 200)
+        self.assertEqual(resp.headers.get("Server"), "http-status-server")
+        self.assertEqual(resp.headers.get("X-HTTP-Status-Server"), "false")
+        self.assertEqual(resp.headers.get("Cache-Control"), "max-age=1")
+        self.assertIn('Date', resp.headers)
+        self.assertEqual(resp.headers.get("Content-Type"), "text/json")
+        self.assertEqual(resp.data, b'{"id": 1}')
+        self.assertEqual(resp.json(), {"id": 1})
+
+        resp = self.res.get_response_to("GOOB", 501)
+        self.assertEqual(resp.status, 501)
+        self.assertEqual(resp.headers.get("Server"), "http-status-server")
+        self.assertIn('Date', resp.headers)
+        self.assertNotIn('Cache-Control', resp.headers)
+        self.assertNotIn('X-HTTP-Status-Server', resp.headers)
+        self.assertEqual(resp.headers.get("Content-Type"), "text/plain")
+        self.assertEqual(resp.data, b"Goob!")
+
+    def test_get_requests_response_to(self):
+        cfg = { "def": { "def": { "body": bodycfg } } }
+        self.res = SimpleResource(cfg)
+        
+        resp = self.res.get_requests_response_to("GET", 200)
+        self.assertTrue(isinstance(resp, requests.Response))
+        self.assertEqual(resp.content, b"it's text")
+        self.assertEqual(resp.text, "it's text")
+
+        cfg.setdefault('headers', {})['Content-Type'] = "text/json"
+        self.res = SimpleResource(cfg)
+        resp = self.res.get_requests_response_to("GET", 200)
+        self.assertEqual(resp.text, '{"a": 1, "b": 2}')
+        self.assertEqual(resp.json(), { "a": 1, "b": 2 })
+
+if __name__ == '__main__':
+    unittest.main()
